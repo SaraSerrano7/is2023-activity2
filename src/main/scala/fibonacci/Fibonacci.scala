06b39726e672d4c455e915f7ec8d7f04efde23fc
SaraSerrano7 <sasecu@gmail.com>
2023-04-26 22:15:31 +0200

    fibonacci implemented and tests passed

--- src/main/scala/fibonacci/Fibonacci.scala
+++ src/main/scala/fibonacci/Fibonacci.scala
@@ -17,6 +17,13 @@ import State.*
 
 def fibonacci(n: Int): Int =
   case class FiboState(i: Int, a: Int, b: Int)
-  lazy val fibonacciAction: State[FiboState, Int] =
-    ???
-  fibonacciAction.run(???)._1
+  lazy val fibonacciAction: State[FiboState, Int] = // Int => (Int, FiboState)
+    get[FiboState].flatMap{
+      case FiboState(i, a, b) => 
+        if (i < n) then
+          set(FiboState(i + 1, b, a + b)).flatMap{
+            _ => fibonacciAction
+          }
+        else unit(a)
+    }
+  fibonacciAction.run(FiboState(0, 0, 1))._1
